b45bdf4b2057eca72f1d6a53e00e284752a78faa
Dunn254 <edwardsbrian070@gmail.com>
2024-07-22 20:28:19 -0700

    restructuring database connection

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const loginController = require('../controllers/loginController');
 const signupController = require('../controllers/signupController');
-const { registerValidation, loginValidation } = require('../validations/userValidation');
+const { registerValidation, loginValidation } = require('../validations/userValidations');
 
 router.post('/signup', registerValidation, signupController.signup);
 router.post('/login', loginValidation, loginController.login);
