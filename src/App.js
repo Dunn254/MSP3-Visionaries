b26b51500cc00f17426e33528540b256aa990461
Van Hoang <hoangduykhanhvan@gmail.com>
2024-07-25 18:38:09 -0700

    add login component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import NavBar from "./components/navbar"
-import Footer from "./components/footer"
-import Signup from "./components/signup"
-
+import NavBar from "./components/navbar" // Import the Navbar component
+import Footer from "./components/footer" // Import the Footer component
+import Signup from "./components/signup" // Import the SignUp component
+import Login from './components/login'; // Import the Login component
 
 function App() {
   return (
