61afe2ed1539b0a35b65268fbf0990dd970057dd
Dunn254 <edwardsbrian070@gmail.com>
2024-07-30 19:52:36 -0700

    footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'; // Ensure this is imported if you have global styles
 import NavBar from './components/navbar'; // Import the NavBar component
-import footer from './components/footer'; // Import the Footer component
+import Footer from './components/Footer'; // Import the Footer component
 import Signup from './components/signup'; // Import the SignUp component
 import Login from './components/login'; // Import the Login component
 import ProductList from './components/ProductList'; 
 import About from './components/About'
 import Home from './components/Home'
 import './index.css'; // Ensure this file exists
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
 function App() {
   return (
     <Router>
